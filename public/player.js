e17c66ea85a5cadaf0465db97e0669258675ba70
Wojciech <wojciech.morkowski@gmail.com>
2019-06-09 17:44:35 +0200

    static files added

---
+++ public/player.js
@@ -0,0 +1,34 @@
+(function () {
+    const manifestUri = 'media/live.mpd';
+
+    function initApp() {
+        shaka.polyfill.installAll();
+
+        if (shaka.Player.isBrowserSupported()) {
+            initPlayer();
+        } else {
+            console.error('Browser not supported!');
+        }
+    }
+
+    function initPlayer() {
+        const video = document.getElementById('video');
+        const player = new shaka.Player(video);
+
+        window.player = player;
+        player.addEventListener('error', onErrorEvent);
+        player.load(manifestUri).then(function () {
+            console.log('The video has now been loaded!');
+        }).catch(onError);
+    }
+
+    function onErrorEvent(event) {
+        onError(event.detail);
+    }
+
+    function onError(error) {
+        console.error('Error code', error.code, 'object', error);
+    }
+
+    document.addEventListener('DOMContentLoaded', initApp);
+})();
